e98a1d47579b9e252da29e2a304f11e22f08d82b
Yutao Meng <yutao.meng@student.uts.edu.au>
2022-05-30 15:07:41 +0800

    Init commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import numpy as np
+from PIL import Image
+
+from transfer_func import run_style_transfer
+
+
+cnt = 0
+
+
+def save_image_array(im: np.ndarray):
+    global cnt
+    im = Image.fromarray(im)
+    im.save(f"{cnt}.png")
+    cnt += 1
+
+
+if __name__ == "__main__":
+    im1 = Image.open("Bangko.jpeg")
+    im2 = Image.open("header.jpg")
+    im, loss = run_style_transfer(
+        content_im=im1,
+        style_im=im2,
+        num_iterations=300,
+        display_interval=100,
+        progress_callback=save_image_array,
+        model_name="vgg",
+        # style_weight=1e2,  # needs different style weight for different models. 1e-3 for vgg and 1e2 for mobilenet
+        # content_weight=1e1, # 1e3 content weight for vgg and 1e1 content weight for mobilenet
+    )
+    Image.fromarray(im).show()
